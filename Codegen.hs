2412665951dab9528a7979805bb6c13275796ce1
Andrey Sverdlichenko <blaze@ruddy.ru>
2014-08-14 07:22:49 +0000

    basic blocks

--- Codegen.hs
+++ Codegen.hs
@@ -4,6 +4,7 @@ import AST
 import LLVM.General.AST
 import LLVM.General.AST.Global
 import LLVM.General.AST.Type
+import Control.Monad.Writer
 import qualified Data.ByteString.Char8 as BS8
 
 genCode :: [Statement] -> Module
@@ -18,7 +19,8 @@ genStatement :: Statement -> Definition
 genStatement (SFunc fname args body) = GlobalDefinition $ functionDefaults {
         returnType = double,
         name = mkName fname,
-        parameters = (map (\n -> Parameter double (mkName n) []) args, False)
+        parameters = (map (\n -> Parameter double (mkName n) []) args, False),
+        basicBlocks = [genBody body]
     }
 genStatement (SExtern fname args) = GlobalDefinition $ functionDefaults {
         returnType = double,
@@ -27,5 +29,13 @@ genStatement (SExtern fname args) = GlobalDefinition $ functionDefaults {
     }
 genStatement (STopLevelExpr expr) = GlobalDefinition functionDefaults {
         returnType = double,
-        name = UnName 0
+        name = UnName 0,
+        basicBlocks = [genBody expr]
     }
+
+funcRet :: Named Terminator
+funcRet = Do (Ret (Just (LocalReference double (Name "retval"))) [])
+
+genBody :: Expr -> BasicBlock
+genBody _ = BasicBlock (Name "entry") [] funcRet
+
